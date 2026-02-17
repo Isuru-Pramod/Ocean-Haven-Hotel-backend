220ed342d89c84a9fa0ddd0bf1e730ae477dd0a0
Isuru-Pramod <mdipromod@gmail.com>
2026-02-17 17:24:47 +0530

    fixed an error

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.analytics import analytics
 
 app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 
 app.register_blueprint(analytics)
 
